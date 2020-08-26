e6a1e20b329fb569f908c871589ab9cd1b0b5025
hudaokeji <github@hudaokeji.com>
2020-08-26 21:56:30 +0900

    update

--- dist/skylark-langx-numbers/numerics.js
+++ dist/skylark-langx-numbers/numerics.js
@@ -5,5 +5,5 @@
  * @link www.skylarkjs.org
  * @license MIT
  */
-define(["skylark-langx-ns","skylark-langx-types"],function(t,e){var n=e.isObject,r=e.isSymbol,i=1/0,a=1.7976931348623157e308,u=NaN,f=/^\s+|\s+$/g,s=/^[-+]0x[0-9a-f]+$/i,o=/^0b[01]+$/i,c=/^0o[0-7]+$/i,l=parseInt;function y(t){return t?(t=g(t))===i||t===-i?(t<0?-1:1)*a:t==t?t:0:0===t?t:0}function g(t){if("number"==typeof t)return t;if(r(t))return u;if(n(t)){var e="function"==typeof t.valueOf?t.valueOf():t;t=n(e)?e+"":e}if("string"!=typeof t)return 0===t?t:+t;t=t.replace(f,"");var i=o.test(t);return i||c.test(t)?l(t.slice(2),i?2:8):s.test(t)?u:+t}return t.attach("langx.numerics",{toFinite:y,toNumber:g,toInteger:function(t){var e=y(t),n=e%1;return e==e?n?e-n:e:0}})});
+define(["skylark-langx-ns","skylark-langx-types"],function(t,e){return t.attach("langx.numerics",{toFinite:e.toFinite,toNumber:e.toNumber,toInteger:e.toInteger})});
 //# sourceMappingURL=sourcemaps/numerics.js.map
